42a5a201d4b32cf3361ff18cad32084507b69ec6
Min <75203074+MinWasTakenOnGitHub@users.noreply.github.com>
2022-09-04 19:14:02 +0700

    Delete Mobile-Scripts directory

--- Mobile-Scripts/exxtremestuffs's Simple Spy (Mobile Support).lua
+++
@@ -1,5 +0,0 @@
-init_remote_spy = loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua"))
-print(pcall(init_remote_spy))
-
--- GG! Arceus X Support
-game:GetService("CoreGui"):WaitForChild("SimpleSpy2"):WaitForChild("Background").Position = UDim2.new(0, 200, 0, 50)
